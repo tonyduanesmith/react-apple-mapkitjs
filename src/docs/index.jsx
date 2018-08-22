9bc57d6c9b3b49f9c0f8dd922e9d9aa32f38a370
Anthony Smith <anthonysmith@anthonys-mbp.home>
2018-08-22 21:54:58 +0100

    basic setup of the wrapper

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { render } from "react-dom";
-import MyComponent from "../../lib";
+import AppleMaps from "../../lib";
 import "./styles.css";
 
 function Demo() {
   return (
-    <div>
-      <h1>Demo with examples of the component</h1>
-      <MyComponent color="brown">Wow what a button</MyComponent>
-    </div>
+    <Fragment>
+      <AppleMaps 
+        width={1000}
+        height={1000}
+        token="eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlRISDlQM1JLTlkifQ.eyJpc3MiOiI3TVE4MjM2RzM0IiwiaWF0IjoxNTM0OTY3Njc1LCJleHAiOjE1Mzc2NDYwODZ9.AZndjH8VBADrpYYHJO4C9OMQ0Yl0Ncr5BgGXcJe2Y08ILeYV6YUK8VdlxnbfoeX83A4kHFasOU-Y54-Ouo1ufQ"
+        longitude={37.3382}
+        latitude={-121.8863}
+        zoomLevel={6}
+      />
+    </Fragment>
   );
 }
 
--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,18 +1,65 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
-class MySuperCoolComponent extends Component {
-  handleClick = () => {
-    console.log("Click!");
-  };
+class AppleMaps extends Component {
+  	componentDidMount(){
+		const { token, longitude, latitude } = this.props
+    	mapkit.init({
+      		authorizationCallback: function (done) {
+        		done(token);
+      		}
+    	});
 
-  render() {
-    const { color, children } = this.props;
-    return (
-      <button onClick={this.handleClick} style={{ color }}>
-        {children}
-      </button>
-    );
-  }
+    	var coords = new mapkit.CoordinateRegion(
+			new mapkit.Coordinate(longitude, latitude),
+      		new mapkit.CoordinateSpan(this.zoomLevel(), this.zoomLevel())
+    	);
+
+    	var map = new mapkit.Map("map");
+    	map.region = coords;
+	}
+
+	zoomLevel(){
+		const { zoomLevel } = this.props
+		console.log(zoomLevel)
+		switch(zoomLevel){
+			case 0:
+				return 300
+			case 1:
+				return 75
+			case 2:
+				return 18.75
+			case 3:
+				return 4.68
+			case 4:
+				return 1.17
+			case 5:
+				return 0.39
+			case 6:
+				return 0.073
+			case 7:
+				return 0.018
+			case 8:
+				return 0.0045
+			default:
+				return 0.35
+		}
+	}
+
+	  
+  	render() {
+    	const { width, height } = this.props
+    	return (
+      		<Fragment>
+        		<div 
+          			id="map" 
+          			style={{
+            			width: width, 
+            			height: height
+          			}}>
+        		</div>
+      		</Fragment>
+    	);
+  	}
 }
 
-export default MySuperCoolComponent;
+export default AppleMaps;
