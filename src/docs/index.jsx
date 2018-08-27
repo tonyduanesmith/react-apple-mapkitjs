b1eed9296bc384fd315876fb4029301bc05bb1a2
tonyduanesmith <tonyduanesmith@me.com>
2018-08-27 22:07:57 +0100

    fixing for git repo

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -3,12 +3,13 @@ import { render } from "react-dom";
 import { AppleMaps, Annotation } from "../../lib";
 import "./styles.css";
 import { CurrentLocationOverride } from "../lib";
+import { YOUR_ACCESS_TOKEN_HERE } from '../../config'
 
 function Demo() {
 	return (
 		<Fragment>
 			<AppleMaps 
-				token="eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlRISDlQM1JLTlkifQ.eyJpc3MiOiI3TVE4MjM2RzM0IiwiaWF0IjoxNTM0OTY3Njc1LCJleHAiOjE1Mzc2NDYwODZ9.AZndjH8VBADrpYYHJO4C9OMQ0Yl0Ncr5BgGXcJe2Y08ILeYV6YUK8VdlxnbfoeX83A4kHFasOU-Y54-Ouo1ufQ"
+				token={YOUR_ACCESS_TOKEN_HERE}
 				longitude={53.8008}
 				latitude={-1.5491}
 				zoomLevel={6}
