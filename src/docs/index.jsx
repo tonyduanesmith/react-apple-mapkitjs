bf586e75d80de29e7b6716e21b0cb1f7b4d2736f
tonyduanesmith <tonyduanesmith@me.com>
2020-02-10 19:14:44 +0000

    fix: Update docs lat lon for coorect lat/lon

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -10,13 +10,13 @@ function Demo() {
 		<Fragment>
 			<AppleMaps 
 				token={YOUR_ACCESS_TOKEN_HERE}
-				longitude={53.8008}
-				latitude={-1.5491}
+				latitude={53.8008}
+				longitude={-1.5491}
 				zoomLevel={6}
 			>
 				<Annotation 
-					longitude={53.8158}
-					latitude={-1.6017}
+					latitude={53.7967}
+					longitude={-1.5438}
 					color="#969696"
 					title="Apple"
 					subtitle="work"
@@ -24,16 +24,16 @@ function Demo() {
 					glyphText=""
 				/>
 				<ImageAnnotation 
-					longitude={53.8158}
-					latitude={-1.4017}
+					latitude={53.8158}
+					longitude={-1.4017}
 					title="Cat Cafe"
 					subtitle="scholes"
 					selected={true}
 					url='./assets/cat.jpeg'
 				/>
 				<Annotation 
-					longitude={53.8058}
-					latitude={-1.5317}
+					latitude={53.7950}
+					longitude={-1.5474}
 					color="red"
 					title="Rail Station"
 					subtitle="Leeds"
@@ -41,8 +41,8 @@ function Demo() {
 					glyphImage='./assets/railicon.png'
 				/>
 				<CurrentLocationOverride 
-					longitude={53.7158}
-					latitude={-1.4017}
+					latitude={53.7158}
+					longitude={-1.4017}
 					direction={90}
 				/>
 			</AppleMaps>
